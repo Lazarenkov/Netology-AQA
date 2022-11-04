29db9654c053394fd8ca1fc4129ebb3bee3cc144
Andrew Lazarenkov <lazarenkovandrew@gmail.com>
2022-11-04 20:26:25 +0300

    feat: headless options set

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,4 +1,45 @@
 package ru.netology;
 
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+import java.util.Set;
+
 public class CardOrderTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void configureWebDriver(){
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "./WebDriver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void startBrowser() {
+        //driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void quitBrowser(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldCreateOrder(){
+        driver.get("http://localhost:7777");
+    }
 }
