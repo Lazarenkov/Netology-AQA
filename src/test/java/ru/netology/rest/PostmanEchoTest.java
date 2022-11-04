d0be0e30d9f761a55da9accdd36d22cc5b2133ee
Andrew Lazarenkov <lazarenkovandrew@gmail.com>
2022-11-04 14:44:57 +0300

    feat: schema validation added

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,17 +1,19 @@
 package ru.netology.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 public class PostmanEchoTest {
+    Request request = new Request();
 
-    @Test
-    void shouldReturnSameJSON() {
-        Request request = new Request();
-
+    @BeforeEach
+    void setRequest() throws JsonProcessingException {
         request.setManufacturer("BMW");
         request.setModel("X3");
         request.setProductionYear(2015);
@@ -19,6 +21,13 @@ public class PostmanEchoTest {
         request.setStateSign("A777AA 77RUS");
         request.setAccidentYears(new int[]{2018, 2020});
 
+//       ObjectMapper mapper = new ObjectMapper();
+//    String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request);
+//    System.out.println(prettyJson);
+    }
+
+    @Test
+    void shouldReturnCorrectFields() {
         given()
                 .contentType(ContentType.JSON)
                 .body(request)
@@ -33,6 +42,19 @@ public class PostmanEchoTest {
                         "data.color", equalTo(request.getColor()),
                         "data.stateSign", containsString("777"),
                         "data.accidentYears", hasItems(2020));
+    }
+
+    @Test
+    void shouldMatchSchema() throws JsonProcessingException {
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .baseUri("https://postman-echo.com")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .assertThat().body(matchesJsonSchemaInClasspath("schema.json"));
     }
 
 }
